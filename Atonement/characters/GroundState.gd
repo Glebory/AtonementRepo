9327c981b16b1099dc22aa7b2348dc30946fbe82
Greg <gleb.vidavski@gmail.com>
2024-01-30 20:23:02 +0000

    Added jump start state
    Jump animation now holds for a time before adding velocity, also letting go of the jump key controls jump height

--- Atonement/characters/GroundState.gd
+++ Atonement/characters/GroundState.gd
@@ -2,10 +2,9 @@ extends State
 
 class_name GroundState
 
-@export var jump_velocity : float = -300.0
 @export var air_state : State
+@export var jump_start_state : State
 @export var jump_animation : String = "jump"
-@export var jump_finish : bool = true
 
 func process(delta):
 	if(!player.is_on_floor()):
@@ -13,9 +12,6 @@ func process(delta):
 		
 func state_input(event : InputEvent):
 	if(event.is_action_pressed("jump")):
-		jump_finish = false
 		playback.travel(jump_animation)
-		await get_tree().create_timer(0.1).timeout
-		player.velocity.y = jump_velocity
-		next_state = air_state
+		next_state = jump_start_state
 		
---
+++ Atonement/characters/JumpStartState.gd
@@ -0,0 +1,23 @@
+extends State
+
+class_name JumpStartState
+
+@export var air_state : State
+@export var jump_velocity : float = -300.0
+@export var jump_an : String
+var beginning : int
+var jump_finish : bool
+
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == jump_an):
+		player.velocity.y = jump_velocity
+		next_state = air_state
+		
+func on_enter():
+	jump_finish = false
+	jump_velocity = -300
+
+func state_input(event: InputEvent):
+	if(event.is_action_released("jump") and not jump_finish and player.velocity.y == 0):
+		jump_velocity = jump_velocity/2
+		jump_finish = true
