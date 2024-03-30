c4817db48eeb20f6525986aeaacc67db398733aa
Greg <gleb.vidavski@gmail.com>
2024-03-30 23:56:11 +0000

    Update hoo_bot.gd
    -created function for consumable drops

--- Atonement/enemies/hoobot/hoo_bot.gd
+++ Atonement/enemies/hoobot/hoo_bot.gd
@@ -1,17 +1,24 @@
 extends CharacterBody2D
 
+signal xp_generated (scene, location, quantity)
+signal creds_generated (scene, location, quantity)
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 @onready var dmgtmr : Timer = $DamagedTimer
-
+#@onready var xp_scene = get_tree().get_first_node_in_group("consumable")
+var cred_scene  = preload("res://ConsumablesAndItems/creds/cred.tscn")
+var xp_scene  = preload("res://ConsumablesAndItems/xp/xp.tscn")
 var speed : float = 50.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2 = Vector2.ZERO
 var dmg : int = 10
+var knockback : int = 30
 var dmging_body : CharacterBody2D
 var entered : bool = false
 @export var health = 100
 @export var full_health = 100
+@export var xp : int = 5
+@export var creds : Array[int] = [2,2]
 
 func _physics_process(delta):
 	if dmgtmr.is_stopped():
@@ -24,7 +31,7 @@ func _physics_process(delta):
 	else:
 		sprite.play("move")
 	if health < 0:
-		queue_free()
+		death()
 
 	move_and_slide()
 	animation_update()
@@ -46,8 +53,13 @@ func hit(dmg, knockback):
 	sprite.self_modulate = Color(1,0,0)
 
 func _on_attackbox_body_entered(body):
-	if body.has_method("hit"):
-		body.hit(dmg) 
+	if body.is_in_group("player"):
+		body.hit(dmg, knockback) 
 
 func _on_damaged_timer_timeout():
 	sprite.self_modulate = Color(1,1,1)
+	
+func death():
+	xp_generated.emit(xp_scene, global_position, xp)
+	creds_generated.emit(cred_scene, global_position, creds)
+	queue_free()
